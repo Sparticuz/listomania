aa48fb1321505577b3ad56eb5f3b1d22090521cf
Sparticuz <kyle@kmcnally.net>
2014-12-09 14:03:20 -0500

    new list interface

--- app/scripts/controllers/new.js
+++ app/scripts/controllers/new.js
@@ -8,13 +8,34 @@
  * Controller of the topAlbumsApp
  */
 angular.module('topAlbumsApp')
-	.controller('NewCtrl', function ($scope) {
+	.controller('NewCtrl', function ($scope,LastFmFactory) {
 		$scope.albums = [{
-			'album':'Test',
-			'artist':'Artist',
-			'thumb':'http://placehold.it/150x150'
+			'album':'Anomaly',
+			'artist':'Test Artist',
+			'comment':'Test Comment',
+			'cover':'http://placehold.it/150x150'
 		}];
 		$scope.addRow = function(){
-			$scope.albums.push({'album':'','thumb':'http://placehold.it/150x150'});
+			$scope.albums.push({
+				'album':'',
+				'artist':'',
+				'comment':'',
+				'cover':'http://placehold.it/150x150'
+			});
+		};
+		$scope.getAlbumArt = function(index){
+			LastFmFactory.getInfo($scope.albums[index].album,$scope.albums[index].artist)
+				.then(function(data){
+					console.log(data);
+					$scope.albums[index].album = data.data.album.name;
+					$scope.albums[index].artist = data.data.album.artist;
+					var entry;
+					for(var n=0;n<data.data.album.image.length;++n){
+						entry = data.data.album.image[n];
+						if (entry.size === 'large'){
+							$scope.albums[index].cover = entry['#text'];
+						}
+					}
+				});
 		};
 	});
