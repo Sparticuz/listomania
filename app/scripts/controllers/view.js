2ea7501794b37d5e5b7501dfee1eb1b159cb9ccf
Sparticuz <kyle@kmcnally.net>
2014-12-09 10:55:39 -0500

    look at route params to determine id of record

--- app/scripts/controllers/view.js
+++ app/scripts/controllers/view.js
@@ -8,8 +8,8 @@
  * Controller of the topAlbumsApp
  */
 angular.module('topAlbumsApp')
-	.controller('ViewCtrl', function ($scope,$route,ListFactory) {
-		ListFactory.getList().then(function(data){
+	.controller('ViewCtrl', function ($scope,$routeParams,ListFactory) {
+		ListFactory.getList($routeParams.id).then(function(data){
 			$scope.albums = data.data.list;
 			$scope.title = data.data.title;
 			//console.log(data);
--- app/scripts/services/ListFactory.js
+++ app/scripts/services/ListFactory.js
@@ -11,8 +11,8 @@
 angular.module('topAlbumsApp')
 	.factory('ListFactory', function ($http) {
 		var factory = {};
-		factory.getList = function() {
-			return $http.get('http://localhost:5984/lists/a4d78c7a7ef976b8ded05f4a23034a9a')
+		factory.getList = function(id) {
+			return $http.get('http://apollo.kmcnally.net:5984/lists/'+id)
 				.success(function(data){
 					return data;
 				});
