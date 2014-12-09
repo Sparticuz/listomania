bcbe1e5b624eaf3f77f405df70e8e6b0dc2618f0
Sparticuz <kyle@kmcnally.net>
2014-12-09 13:40:14 -0500

    refactored listfactory

--- app/scripts/services/ListFactory.js
+++ app/scripts/services/ListFactory.js
@@ -1,22 +1,23 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name topAlbumsApp.services:ListFactory
+ * @ngdoc service
+ * @name topAlbumsApp.ListFactory
  * @description
- * # couch factory
- * Factory of the topAlbumsApp
+ * # ListFactory
+ * Factory in the topAlbumsApp.
  */
-
 angular.module('topAlbumsApp')
 	.factory('ListFactory', function ($http) {
-		var factory = {};
-		factory.getList = function(id) {
-			return $http.get('http://apollo.kmcnally.net:5984/lists/'+id)
+		var BASE_URL = 'http://apollo.kmcnally.net:5984/';
+		//Public API
+		return {
+			getList : function(id) {
+				var METHOD = 'lists/';
+				return $http.get(BASE_URL+METHOD+id)
 				.success(function(data){
 					return data;
 				});
+			}
 		};
-		return factory;
-	}
-);
+	});
