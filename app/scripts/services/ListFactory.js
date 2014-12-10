0df609cbf7a42a72eb30125fd6d53ad526d16b3f
Sparticuz <kyle@kmcnally.net>
2014-12-09 23:44:02 -0500

    list factory stuff

--- app/scripts/services/ListFactory.js
+++ app/scripts/services/ListFactory.js
@@ -10,11 +10,31 @@
 angular.module('topAlbumsApp')
 	.factory('ListFactory', function ($http) {
 		var BASE_URL = 'http://apollo.kmcnally.net:5984/';
+		var TABLE = 'lists/';
 		//Public API
 		return {
 			getList : function(id) {
-				var METHOD = 'lists/';
-				return $http.get(BASE_URL+METHOD+id)
+				return $http.get(BASE_URL+TABLE+id)
+				.success(function(data){
+					return data;
+				});
+			},
+			putList : function(item){
+				return $http.post(BASE_URL+TABLE,item)
+				.success(function(data){
+					console.log(data);
+					return data;
+				});
+			},
+			deleteList : function(id,rev){
+				return $http.delete(BASE_URL+TABLE+id+'?rev='+rev)
+				.success(function(data){
+					console.log(data);
+					return data;
+				});
+			},
+			allLists : function(){
+				return $http.get(BASE_URL+TABLE+'_all_docs')
 				.success(function(data){
 					return data;
 				});
