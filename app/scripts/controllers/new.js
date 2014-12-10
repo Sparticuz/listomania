7a74008c77f54eff8619479e026375e922d46e76
Sparticuz <kyle@kmcnally.net>
2014-12-09 23:43:39 -0500

    clean entry before post

--- app/scripts/controllers/new.js
+++ app/scripts/controllers/new.js
@@ -8,32 +8,44 @@
  * Controller of the topAlbumsApp
  */
 angular.module('topAlbumsApp')
-	.controller('NewCtrl', function ($scope,LastFmFactory) {
-		$scope.albums = [{
+	.controller('NewCtrl', function ($scope,$location,ListFactory,LastFmFactory) {
+		$scope.list = {};
+		$scope.list.albums = [{
 			'album':'Anomaly',
 			'artist':'Test Artist',
 			'comment':'Test Comment',
-			'cover':'http://placehold.it/150x150'
+			'cover':'http://placehold.it/132x132'
 		}];
 		$scope.addRow = function(){
-			$scope.albums.push({
+			$scope.list.albums.push({
 				'album':'',
 				'artist':'',
 				'comment':'',
-				'cover':'http://placehold.it/150x150'
+				'cover':'http://placehold.it/132x132'
+			});
+		};
+		$scope.processList = function() {
+			angular.forEach($scope.list.albums, function(value,key){
+				//remove blank's
+				if(value.album === '' && value.artist === ''){
+					$scope.list.albums.splice(key,1);
+				}
+			});
+			ListFactory.putList($scope.list).then(function(data){
+				$location.url('/view/'+data.data.id);
 			});
 		};
 		$scope.getAlbumArt = function(index){
-			LastFmFactory.getInfo($scope.albums[index].album,$scope.albums[index].artist)
+			LastFmFactory.getInfo($scope.list.albums[index].album,$scope.list.albums[index].artist)
 				.then(function(data){
 					console.log(data);
-					$scope.albums[index].album = data.data.album.name;
-					$scope.albums[index].artist = data.data.album.artist;
+					$scope.list.albums[index].album = data.data.album.name;
+					$scope.list.albums[index].artist = data.data.album.artist;
 					var entry;
 					for(var n=0;n<data.data.album.image.length;++n){
 						entry = data.data.album.image[n];
-						if (entry.size === 'large'){
-							$scope.albums[index].cover = entry['#text'];
+						if (entry.size === 'mega'){
+							$scope.list.albums[index].cover = entry['#text'];
 						}
 					}
 				});
